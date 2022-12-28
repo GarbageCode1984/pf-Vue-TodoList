df013beae3e1e8000b4b96b687e9b676fa356b08
GarbaGeCode1984 <rkqlwl1984@gmail.com>
2022-12-28 12:37:24 +0900

    Good

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      placeholder="Type what you have to do"
+      v-model="newTodoItem"
+      v-on:keyup.enter="addTodo"
+    />
+    <span v-on:click="addTodo">
+      <i class="fa-solid fa-plus"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { newTodoItem: "" };
+  },
+  methods: {
+    addTodo() {
+      //공백도 입력되는 상황을 막아주기 위해
+      if (this.newTodoItem !== "") {
+        let value = this.newTodoItem && this.newTodoItem.trim();
+        this.$emit("addTodo", value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      //인풋박스 입력후 초기화
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+span {
+  float: right;
+  background: linear-gradient(to right, #6677ff, #885bc7);
+  width: 45px;
+  border-radius: 0 5px 5px 0;
+}
+input {
+  border: none;
+  font-size: 1rem;
+  text-align: center;
+  width: calc(90% - 45px);
+}
+input:focus {
+  outline: none;
+}
+i {
+  color: white;
+  vertical-align: middle;
+}
+</style>
